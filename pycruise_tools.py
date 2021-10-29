c8594cf95c278ae8c12af19194e7859dec624c4d
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-10-29 10:02:49 +0200

    Add lon

--- pycruise_tools.py
+++ pycruise_tools.py
@@ -29,17 +29,37 @@ def parse_latitude_text(lat_text):
     # Parse decimal degrees format --- latitude
     else:
         lat = float(lat_text)
-        
+
     return lat
 
 
 def parse_longitude_text(lon_text):
-    
-    try:
+    """Convert longitude as a text string in various formats to
+    a float of decimal degrees.
+    """
+
+    # Define all longitude formats
+    lon_dmf = re.match("(\d+)°(\d+\.\d+)'([EW])", lon_text)
+    lon_dmsf = re.match("(\d+)°(\d+)'(\d+)\"([EW])", lon_text)
+
+    # Parse decimal minutes format --- longitude
+    if lon_dmf:
+        lon = float(lon_dmf.group(1)) + float(lon_dmf.group(2)) / 60
+        if lon_dmf.group(3) == "W":
+            lon *= -1
+
+    # Parse degrees minutes seconds format --- longitude
+    elif lon_dmsf:
+        lon = (
+            float(lon_dmsf.group(1))
+            + float(lon_dmsf.group(2)) / 60
+            + float(lon_dmsf.group(3)) / 3600
+        )
+        if lon_dmsf.group(4) == "W":
+            lon *= -1
+
+    # Parse decimal degrees format --- longitude
+    else:
         lon = float(lon_text)
-    except:
-        lon = None
-        
+
     return lon
-    
-    
